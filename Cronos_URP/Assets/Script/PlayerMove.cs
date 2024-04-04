9dc253d09428d32aa851d0f916532f7ff8b28a52
OHK <60260523+Haekang-sound@users.noreply.github.com>
2024-04-04 20:18:25 +0900

    fix unity와 비주얼스튜디오가 연결되지 않는 오류 해결

--- Cronos_URP/Assets/Script/PlayerMove.cs
+++ Cronos_URP/Assets/Script/PlayerMove.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 	// 필요한 변수
-
 	public float speed;
 	float hAxis;
 	float vAxis;
-	bool wDown;
+	bool wRun;
+	Transform PlayerCAMRoot;	// 카메라루트를 담을 ? 
+	Animator m_animator;		// 애니메이터정보를 가져와서 반영하자
 
 	Vector3 moveVec;
 	Animator anim;
 
+	private void Awake()
+	{
+		// 플레이어 카메라의 트랜스폼을 가져온다.
+		PlayerCAMRoot = GetComponentsInChildren<Transform>()[0];
+	}
+
 	void Start()
 	{
-		// anim = GetComponentInChildren<Animator>();
+		anim = GetComponentInChildren<Animator>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		hAxis = Input.GetAxisRaw("Horizontal");	// GetAxisRaw는 -,0,+ 값만 가져온다(방향만 가져옴)
-		vAxis = Input.GetAxisRaw("Vertical");
-		// wDown = Input.GetButton("Walk"); // 동작여부를 받는다.
+		// GetAxisRaw는 -,0,+ 값만 가져온다(방향만 가져옴)
+		hAxis = Input.GetAxisRaw("Horizontal");	// x축에 수평으로 움직인다.
+		vAxis = Input.GetAxisRaw("Vertical");	// x축에 수직으로 움직인다.
+
+		wRun = Input.GetButton("Run"); // 동작여부를 받는다.
 
 		// 높이의 대한 이동이 없으니 0을 넣는다.
 		moveVec = new Vector3(hAxis, 0, vAxis).normalized;
