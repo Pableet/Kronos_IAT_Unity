294979ec1de85d23151eb730c26445a2338f06f1
napseed <xortggg@gmail.com>
2024-04-25 16:14:36 +0900

    add HitStop Script

---
+++ Cronos_URP/Assets/Script/HitStop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitStop : MonoBehaviour
+{
+    public float hitStopTime = 0.0f;
+    Animator animator;
+    bool isHit;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        isHit = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isHit == false)
+        {
+            isHit = true;
+            Debug.Log("Testing HitStop");
+            StartCoroutine(HitStopCoroutine());
+        }
+    }
+
+    IEnumerator HitStopCoroutine()
+    {
+        animator.speed = 0.0f;
+        yield return new WaitForSecondsRealtime(hitStopTime);
+        animator.speed = 1.0f;
+        isHit = false;
+    }
+}
