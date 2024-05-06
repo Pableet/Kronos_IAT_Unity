056dcb57632592b6a21a71f31453121685b3063c
DESKTOP-37BOMML\sunri <sunriver94@gmail.com>
2024-05-07 04:09:07 +0900

    add 몬스터매니저

--- Cronos_URP/Assets/Script/MonsterSelector.cs
+++ Cronos_URP/Assets/Script/MonsterSelector.cs
@@ -4,64 +4,57 @@ using System.Reflection;
 using UnityEditor.Rendering;
 using UnityEngine;
 
+/// <summary>
+/// 몬스터 셀렉터는 이제 사용하지 않는다
+/// </summary>
 public class MonsterSelector : MonoBehaviour
 {
 
-	public Transform Monsetr0;
-	public Transform Monsetr1;
-	public Transform Monsetr2;
-	public Transform Monsetr3;
-
 	// 몬스터를 관리할 목록
-	List<Transform> monsters = new List<Transform>();
+	List<GameObject> monsters;
 
 	// 오토 타겟에 그걸 해주자
 	public AutoTargetting autoTargetor;
 
 
 	float? min = null;
-	int indexNum;
-
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		// 일단 담아
-		monsters.Add(Monsetr0);
-		monsters.Add(Monsetr1);
-		monsters.Add(Monsetr2);
-		monsters.Add(Monsetr3);
+		monsters = MonsterManager.Instance.list;
+		
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.I))
-		{
-			FindTarget();
+    public bool FindTarget()
+    {
+        // 몬스터리스트가 없거나 사이즈가 0이라면 false
+        if (monsters == null || monsters.Count == 0)
+        {
+            return false;
+        }
+        // 현재 몬스터 목록을 순회한다.
+        for (int i = 0; i < monsters.Count; i++)
+        {
+            // 몬스터와 플레이어 사이의 거리벡터의 크기를 구한다.
+            /// 몬스터의 트랜스폼을 
+            /// 매번 컴포넌트 접근으로 찾아야하는 점은 
+            /// 너무 아쉬운 일이다. 방법을 찾아볼까?
+            /// 1. findTarget기능의 위치를 바꾼다.
+            float value = Mathf.Abs((autoTargetor.PlayerObject.position - monsters[i].GetComponent<Transform>().position).magnitude);
 
-		}
-	}
+            // 현재 min값보다 value가 작다면 혹은 min에 값이 들어있지 않다면 
+            if (min > value || min == null)
+            {
+                // min 값을 교체하고 
+                min = value;
 
-	public void FindTarget()
-	{
-		// 현재 몬스터 목록을 순회한다.
-		for (int i = 0; i < monsters.Count; i++)
-		{
-			// 몬스터와 플레이어 사이의 거리벡터의 크기를 구한다.
-			float value = Mathf.Abs((autoTargetor.PlayerObject.position - monsters[i].position).magnitude);
+                // 가장 작은 값을 가진 트랜스폼을 타겟으로 설정한다.
+                autoTargetor.Target = monsters[i].GetComponent<Transform>();
+            }
+        }
 
-			// 현재 min값보다 value가 작다면 혹은 min에 값이 들어있지 않다면 
-			if (min > value || min == null)
-			{
-				// min 값을 교체하고 
-				/// 문제발견, 한 번 min으로 등록된 값이 새로 바뀌지 않는다.
-				min = value;
-				// 가장 작은 값을 가진 트랜스폼을 타겟으로 설정한다.
-				autoTargetor.Target = monsters[i];
-			}
-		}
-
-		min = null;
-	}
+        min = null;
+        return true;
+    }
 }
