f0ddc93f88b65267b5fee17c445df08a6d3f54fa
OHK <60260523+Haekang-sound@users.noreply.github.com>
2024-05-15 21:13:42 +0900

    feat 커밋

--- Cronos_URP/Assets/Script/PlayerData.cs
+++ Cronos_URP/Assets/Script/PlayerData.cs
@@ -3,13 +3,13 @@
 using System.Numerics;
 using UnityEngine;
 
-class PlayerData
+public class PlayerData
 {
 	// 저장할 데이터를 하나씩 늘이면 될듯
 	public string saveScene { get; set; }
 	public float CP { get; set; }
 	public float TP { get; set; }
-	public UnityEngine.Vector3 RespawnPos { get; set; }
+	public UnityEngine.Vector3 RespawnPos { get; set; } = new UnityEngine.Vector3(0f,0f,0f);
 
 }
 
