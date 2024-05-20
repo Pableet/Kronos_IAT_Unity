8b5391b541b49981c77b2156b4fccde8f5bcc8e1
napseed <xortggg@gmail.com>
2024-05-20 14:17:01 +0900

    데모 증강 구현

--- Cronos_URP/Assets/UI/UI_PointText.cs
+++ Cronos_URP/Assets/UI/UI_PointText.cs
@@ -11,19 +11,26 @@ public class UI_PointText : UI_TPCP
     public TextMeshProUGUI textTP;
     public TextMeshProUGUI textCP;
 
+    Player player;
+    float tp;
+    float cp;
 
     private void Start()
     {
         Bind<TextMeshProUGUI>(typeof(Texts));
+        player = GameObject.Find("Player").GetComponent<Player>();
+
+        if (player == null)
+        {
+            Debug.Log("플레이어가 없당께요");
+        }
     }
 
-    int tp = 99;
-
-    public void OnButtonClicked()
+    private void Update()
     {
-        Debug.Log("Button Clicked");
-
-        tp--;
-        textTP.text = tp.ToString("0000");
+        tp = player.currentTP;
+        cp = player.CP;
+        textTP.text = "TP:" + tp.ToString("0000");
+        textCP.text = "CP:" + cp.ToString("0000");
     }
 }
