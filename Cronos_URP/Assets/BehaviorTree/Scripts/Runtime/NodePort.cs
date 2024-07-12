c4f8fa902192910f1d7bf4b01b101d79283c2449
Leusin <leusin@naver.com>
2024-07-12 19:03:07 +0900

    fix: build가 되도록 세팅

--- Cronos_URP/Assets/BehaviorTree/Scripts/Runtime/NodePort.cs
+++ Cronos_URP/Assets/BehaviorTree/Scripts/Runtime/NodePort.cs
@@ -6,16 +6,18 @@
  * 'NodePort' 클래스와 'DefaultEdgeConnectorListener' 내부 클래스는 Unity 에디터 내에서 사용자 정의 그래프 뷰를 구현할 때,
  * 노드 간의 연결을 쉽게 만들고 관리할 수 있는 기능을 제공한다.
  */
-
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine.UIElements;
+#if UNITY_EDITOR
+using UnityEditor.Experimental.GraphView;
+#endif
 
 /// <summary>
 /// 노드 간의 연결점을 나타매여, 연결의 방향과 용량을 지정할 수 있다.
 /// 이 클래스는 그래프 뷰 내에서 노드 간의 연결을 생성하고 관리하기 위한 기능을 제공한다.
 /// </summary>
+#if UNITY_EDITOR
 public class NodePort : Port
 {
 
@@ -104,3 +106,4 @@ public class NodePort : Port
         return rect.Contains(localPoint);
     }
 }
+#endif
