97009eb56e1c7a5f0087a627f54bd31da95b408e
Leusin <leusin@naver.com>
2024-07-15 17:47:17 +0900

    fix: 수정된 s attack pursuit 상태에서 교착되는 오류 수정

--- Cronos_URP/Assets/Script/npcAI/behavior/ATypeEnemy/ATypeEnemySMBPursuit.cs
+++ Cronos_URP/Assets/Script/npcAI/behavior/ATypeEnemy/ATypeEnemySMBPursuit.cs
@@ -4,12 +4,19 @@ using UnityEngine.InputSystem.XR;
 
 public class ATypeEnemySMBPursuit : SceneLinkedSMB<ATypeEnemyBehavior>
 {
-    public bool sAttack;
+    public enum PursuitFor
+    {
+        Normal,
+        SAttack
+    }
+    public PursuitFor purpose;
+
 
     public override void OnSLStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _monoBehaviour.ChangeDebugText("PURSUIT");
 
+        _monoBehaviour.StartPursuit();
         _monoBehaviour.Controller.SetFollowNavmeshAgent(true);
 
         // Damaged - 상태에서 피격 당했을 때
@@ -20,7 +27,7 @@ public class ATypeEnemySMBPursuit : SceneLinkedSMB<ATypeEnemyBehavior>
     {
         _monoBehaviour.FindTarget();
 
-         // 경로를 찾을 수 없을 때
+        // 경로를 찾을 수 없을 때
         if (_monoBehaviour.Controller.navmeshAgent.pathStatus == NavMeshPathStatus.PathPartial
             || _monoBehaviour.Controller.navmeshAgent.pathStatus == NavMeshPathStatus.PathInvalid)
         {
@@ -28,30 +35,42 @@ public class ATypeEnemySMBPursuit : SceneLinkedSMB<ATypeEnemyBehavior>
             return;
         }
 
-        if (_monoBehaviour.CurrentTarget != null )
+        if (_monoBehaviour.CurrentTarget != null)
         {
             _monoBehaviour.RequestTargetPosition();
 
-            // STRONG ATTACK - 공격 사거리 안에 있고, SAttack으로 이동해야 하는 경우
-            if (_monoBehaviour.IsInStrongAttackRange() && sAttack == true)
+            if (purpose == PursuitFor.Normal)
             {
-                _monoBehaviour.TriggerStrongAttack();
-            }
-            // ATTACK - 공격 사거리 안에 있을 때
-            else if (_monoBehaviour.IsInAttackRange())
-            {
-                _monoBehaviour.TriggerAttack();
-            }
-            // PURSUIT - 공격 위치를 할당 받았을 때
-            else if (_monoBehaviour.FollowerData.assignedSlot != -1)
-            {
-                Vector3 targetPoint = _monoBehaviour.FollowerData.requiredPoint;
+                // ATTACK - 공격 사거리 안에 있을 때
+                if (_monoBehaviour.IsInAttackRange())
+                {
+                    _monoBehaviour.TriggerAttack();
+                }
+                // PURSUIT - 공격 위치를 할당 받았을 때
+                else if (_monoBehaviour.FollowerData.assignedSlot != -1)
+                {
+                    Vector3 targetPoint = _monoBehaviour.FollowerData.requiredPoint;
 
-                _monoBehaviour.Controller.SetTarget(targetPoint);
+                    _monoBehaviour.Controller.SetTarget(targetPoint);
+                }
+                else // Strafe - 타깃을 찾을 수 있을 때
+                {
+                    _monoBehaviour.TriggerStrafe();
+                }
             }
-            else // Strafe - 타깃을 찾을 수 있을 때
+            // STRONG ATTACK - 공격 사거리 안에 있고, SAttack으로 이동해야 하는 경우
+            else if (purpose == PursuitFor.SAttack)
             {
-                _monoBehaviour.TriggerStrafe();
+                if (_monoBehaviour.IsInStrongAttackRange())
+                {
+                    _monoBehaviour.TriggerStrongAttack();
+                }
+                else if (_monoBehaviour.FollowerData.assignedSlot != -1)
+                {
+                    Vector3 targetPoint = _monoBehaviour.FollowerData.requiredPoint;
+
+                    _monoBehaviour.Controller.SetTarget(targetPoint);
+                }
             }
         }
         // IDLE - 그 외(아직 플레이어 사망은 미포함)
